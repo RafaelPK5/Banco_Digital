1d4db332f51c677957344e54f7f12ad29ca524f8
RafaelPK5 <contato.rafaelsantanak5@gmail.com>
2023-05-18 21:36:43 -0300

    Pequena mudança no arquivo de conexão ao DB

--- db/conect.js
+++ db/conect.js
@@ -2,10 +2,10 @@ import mysql from 'mysql2'
 import env from 'dotenv'
 env.config()
 
-const conection = await mysql.createConnection({
-    host:'127.0.0.1',
+const conection = mysql.createConnection({
+    host:process.env.IP,
     user:'root',
-    password:'PyKiller@2905',
+    password:process.env.PASSWORD,
     database:'banco_digital'
 })
 
